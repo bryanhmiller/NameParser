5fe1a3d2a39a11077250c8dbf65797b1a47f1c6f
Nathan Gonzalez <nathan@copypastedeveloper.com>
2017-07-29 11:32:10 -0500

    console input/output

--- NameParser.App/Program.cs
+++ NameParser.App/Program.cs
@@ -6,7 +6,44 @@ namespace NameParser.App
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var menu = new [] {"First Option","Second Option", "Third Option"};
+
+            Console.WriteLine("Please enter your name!");            
+
+            var parser = new Parser();
+            var name = Console.ReadLine();        
+
+            Console.WriteLine($"Hi, {name}!");
+
+            var user = parser.ParseName(name);
+
+            Console.BackgroundColor = ConsoleColor.Magenta;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Your first name is {user.FirstName}!{Environment.NewLine}");                                        
+            
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"Your last name is {user.LastName}!");
+
+            Console.Beep();
+
+            Console.WriteLine("Press enter to continue");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("Choose an option:");            
+            foreach (var item in menu)
+            {
+                Console.WriteLine($"{Array.IndexOf(menu,item)+1}.) {item}");
+            }
+
+            var choice = Console.ReadKey();
+
+            Console.Clear();
+            Console.WriteLine($"You chose {menu[int.Parse(choice.KeyChar.ToString())-1]}");
+            
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 }
